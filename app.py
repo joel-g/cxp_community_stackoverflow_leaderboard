1e62f1bca4f0819e4e03ee640e7e22a7fc2ee8b8
joel-g <joelaguerra1@gmail.com>
2018-05-18 15:19:14 -0700

    adds some users

--- app.py
+++ app.py
@@ -3,12 +3,11 @@ import requests
 
 app = Flask(__name__)
 
-STACK_IDS=('8826629', '9541448')
+STACK_IDS=('8826629', '9541448', '6591675', '7452904', '9393813', '8840926')
 
 def get_user(user_id):
     res = requests.get("http://api.stackexchange.com/2.2/users/" + user_id + "?order=desc&sort=reputation&site=stackoverflow")
     return res.json()['items'][0]
-
 
 @app.route('/')
 def index(users=None):
