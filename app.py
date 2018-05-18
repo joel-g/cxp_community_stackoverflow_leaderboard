ed0262f51e0f8bb84373b0cfbe17dafb3a8496af
joel-g <joelaguerra1@gmail.com>
2018-05-18 15:13:57 -0700

    working leaderboard

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
+import requests
+
 app = Flask(__name__)
 
+STACK_IDS=('8826629', '9541448')
+
+def get_user(user_id):
+    res = requests.get("http://api.stackexchange.com/2.2/users/" + user_id + "?order=desc&sort=reputation&site=stackoverflow")
+    return res.json()['items'][0]
+
+
 @app.route('/')
-def index():
-    return render_template('index.html', name=name)
+def index(users=None):
+    jsons = []
+    for user_id in STACK_IDS:
+        jsons.append(get_user(user_id))
+    jsons = sorted(jsons, key=lambda k: k['reputation_change_week'], reverse=True)
+    return render_template('index.html', users=jsons)
