876a6757f7fede3f88d058f5385a19bad774aac8
joel-g <joelaguerra1@gmail.com>
2018-05-19 15:50:15 -0700

    add more users

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ import requests
 
 app = Flask(__name__)
 
-STACK_IDS=('8826629','9541448', '6591675', '7452904', '9393813', '8840926', '9454112', '7287446', '9598801', '9541439')
+STACK_IDS=('8826629','9541448', '6591675', '7452904', '9393813', '8840926', '9454112', '7287446', '9598801', '9541439', '9554961', '8797362', '8748848', '9672092', '9603809', '9536851', '8366814', '9541510', '9541342')
 
 def get_user(user_id):
     res = requests.get("http://api.stackexchange.com/2.2/users/" + user_id + "?order=desc&sort=reputation&site=stackoverflow")
     return res.json()['items'][0]
 
-def pagify_users(order_by):
+def rank_users(order_by):
     jsons = []
     for user_id in STACK_IDS:
         jsons.append(get_user(user_id))
@@ -18,17 +18,17 @@ def pagify_users(order_by):
 
 @app.route('/')
 def index(users=None):
-    jsons = pagify_users("reputation_change_week")
-    return render_template('index.html', users=jsons, title="Rep Change this Week")
-
-@app.route('/total')
-def total(users=None):
-    jsons = pagify_users("reputation")
+    jsons = rank_users("reputation")
     return render_template('index.html', users=jsons, title="Total Reputation")
+
+@app.route('/week')
+def total(users=None):
+    jsons = rank_users("reputation_change_week")
+    return render_template('index.html', users=jsons, title="Rep Change this Week") 
 
 @app.route('/month')
 def month(users=None):
-    jsons = pagify_users("reputation_change_month")
+    jsons = rank_users("reputation_change_month")
     return render_template('index.html', users=jsons, title="Rep Change this Month")
 
 if __name__ == '__main__':
