bc0df75c36ff77e0d71c5d609d25695b03c821a8
joel-g <joelaguerra1@gmail.com>
2018-07-25 11:53:28 -0700

    reduces API calls from N to 1

--- index.py
+++ index.py
@@ -5,13 +5,20 @@ app = Flask(__name__)
 
 STACK_IDS=('8826629','9541448', '6591675', '7452904', '9393813', '8840926', '9454112', '9598801', '8797362', '8748848', '9672092', '9603809', '9536851', '8366814', '9541342', '8840650', '9873161', '9401038', '5420074')
 
-def get_user(user_id):
-    res = requests.get("http://api.stackexchange.com/2.2/users/" + user_id + "?order=desc&sort=reputation&site=stackoverflow&key=dS6SG)8rCYRWj0DcXzmJ4w((")
-    return res.json()['items'][0]
-    # Don't steal my API key. Just get your own free one.
+def form_api_call(stack_ids):
+    url = "http://api.stackexchange.com/2.2/users/"
+    for user in stack_ids:
+        url = url + user + ";"
+    url = url[:-1]
+    url = url + "?order=desc&sort=reputation&site=stackoverflow&key=dS6SG)8rCYRWj0DcXzmJ4w(("
+    return url
+
+def get_users():
+    res = requests.get(form_api_call(STACK_IDS))
+    return res.json()['items']
 
 def rank_users(order_by):
-    jsons = map(get_user, STACK_IDS)
+    jsons = get_users()
     jsons = sorted(jsons, key=lambda k: k[order_by], reverse=True)
     return jsons
 
